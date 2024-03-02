ab55a92b5c4f472831143b06cecaf1f3550a5701
bradefaire <123887842+bradefaire@users.noreply.github.com>
2024-03-02 20:19:38 +0100

    création du terrain et telegargement d'asset 1er proto

---
+++ Assets/Scripts/Characters/PlayerData.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PlayerData : ScriptableObject
+{
+    [Range(1, 20)]
+    [Tooltip("Vitesse")]
+    public int Speed = 1;
+
+    [Range(1, 20)]
+    [Tooltip("jumpForce")]
+    public int JumpForce = 5;
+
+    [Range(1, 20)]
+    [Tooltip("forceGravity")]
+    public int ForceGravity = 10;
+
+    [Range(20, 1000)]
+    [Tooltip("mouseSensitivity")]
+    public int MouseSensitivity = 1;
+
+    [Range(-45, -20)]
+    [Tooltip("minAngleCamera")]
+    public int MinLookAngle = -30;
+
+    [Range(20, 45)]
+    [Tooltip("maxAngleCamera")]
+    public int MaxLookAngle = 35;
+
+    [Tooltip("invertCamera")]
+    public bool invertCamera = false;
+
+
+}
